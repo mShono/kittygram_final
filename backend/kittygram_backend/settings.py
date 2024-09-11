62c4efa430b3ced6e85b45c7be72deac50af8997
Mariia Samokhina <mshono@yandex.ru>
2024-09-11 13:54:42 +0300

    Removed the comments

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,12 +3,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-
-# DEBUG = True
-
-# ALLOWED_HOSTS = ['89.169.168.19', '127.0.0.1', 'localhost', 'ginjx.hopto.org']
-
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 DEBUG = bool(os.getenv('DEBUG', True))
